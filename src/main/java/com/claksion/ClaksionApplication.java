6af5fc75de77a50c9ad8b439f48e7028b54e9e99
hyerimmy <hyerim2000@swu.ac.kr>
2024-04-24 15:22:02 +0900

    [feat] 프로젝트 셋팅

---
+++ src/main/java/com/claksion/ClaksionApplication.java
@@ -0,0 +1,13 @@
+package com.claksion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaksionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaksionApplication.class, args);
+    }
+
+}
