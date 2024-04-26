b786bb53cd93db7ecc9e26bd971e3532592fe19c
Wonhee0221 <totohwh3601@naver.com>
2024-04-26 16:06:39 +0900

    [feat] properties 설정

--- src/test/java/com/claksion/ClaksionApplicationTests.java
+++ src/test/java/com/claksion/ClaksionApplicationTests.java
@@ -1,13 +1,16 @@
 package com.claksion;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class ClaksionApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("TEST-------------------");
     }
 
 }
