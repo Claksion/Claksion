63f581cfafcbaff03de5d4362f8ebea07ea95fd1
Wonhee0221 <totohwh3601@naver.com>
2024-04-26 17:40:05 +0900

    [feat] 간단한index화면 설정

---
+++ src/main/java/com/claksion/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.claksion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String main(Model model){
+        return "index";
+    }
+
+}
