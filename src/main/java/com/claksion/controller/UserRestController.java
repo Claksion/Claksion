f48add12f93eaecf63105dd5e586e4bb8c6bd81e
hyerimmy <hyerim2000@swu.ac.kr>
2024-05-07 17:44:55 +0900

    [feat] 홈화면 사용자 로그인 정보 실시간 - SSE 단방향 통신 사용

--- src/main/java/com/claksion/controller/UserRestController.java
+++ src/main/java/com/claksion/controller/UserRestController.java
@@ -1,12 +1,21 @@
 package com.claksion.controller;
 
+import com.claksion.app.data.dto.ClassMate;
 import com.claksion.app.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,9 +24,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
 
     final private UserService userService;
+    final RedisTemplate<String, Object> redisTemplate;
 
-//    @RequestMapping(value = "/register", method = RequestMethod.POST)
-//    public String register(Model model) {
-//        return "register";
-//    }
+    final ExecutorService executor = Executors.newCachedThreadPool();
+
+    @GetMapping("/get")
+    public SseEmitter get(@RequestParam("classroomId") Integer classroomId) throws Exception {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        executor.execute(() -> {
+            try {
+                while (true) {
+                    List<ClassMate> classMates = userService.getClassMates(classroomId);
+                    Map<Integer, Boolean> onlineState = new HashMap<>();
+
+                    for (ClassMate mate : classMates) {
+                        int userId = mate.getId();
+                        boolean isOnline = Boolean.TRUE.equals(redisTemplate.hasKey("loginUser:" + userId));
+                        onlineState.put(userId, isOnline);
+                    }
+                    emitter.send(onlineState);
+
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                emitter.completeWithError(e);
+            }
+        });
+        return emitter;
+    }
 }
